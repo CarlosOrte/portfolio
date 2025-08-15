7b26fbd34ef91255db4c8e8dc7e044e42cc9bce2
CarlosOrte <al168703@alumnos.uacj.mx>
2025-08-15 09:33:37 -0600

    actualizar etilos para estrella holografica

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -24,6 +24,15 @@ const Data = () => {
                     xmlns="http://www.w3.org/2000/svg"
                     className="home__hand" 
                 >
+                  <defs>
+                    <linearGradient id="holographic-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                      <stop offset="0%" style={{stopColor:'red', stopOpacity:'1'}} />
+                      <stop offset="25%" style={{stopColor:'blue', stopOpacity:'1'}} />
+                      <stop offset="50%" style={{stopColor:'green', stopOpacity:'1'}} />
+                      <stop offset="75%" style={{stopColor:'purple', stopOpacity:'1'}} />
+                      <stop offset="100%" style={{stopColor:'red', stopOpacity:'1'}} />
+                    </linearGradient>
+                  </defs>
                   {/* CAMBIO: Agrega una nueva clase para la animación */}
                   <path d="M12 0L14.71 9.29L24 12L14.71 14.71L12 24L9.29 14.71L0 12L9.29 9.29L12 0Z" className="holographic-path" /> 
                 </svg>
